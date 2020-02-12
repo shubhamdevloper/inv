dfbd926929034e2c068358b030b10d78afe059b3
shubhamdevloper <45872629+shubhamdevloper@users.noreply.github.com>
2020-02-12 17:37:08 +0530

    Add files via upload
    hh

---
+++ Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <hnavbar></hnavbar>
+    <div class="background-img">
+    </div>
+  </div>
+</template>
+
+<script>
+import Hnavbar from '../components/Hnavbar.vue'
+export default {
+  name: 'home',
+  components: {
+    'hnavbar': Hnavbar
+  }
+}
+</script>
+<style lang="scss">
+body{
+    margin:0px;
+}
+.background-img {
+  background-image: url("../assets/bg-image.jpg");
+  background-repeat: repeat-x;
+  animation: slideleft 40000s infinite linear;
+  -webkit-animation: slideleft 40000s infinite linear;
+  background-size: cover;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  background-attachment: fixed;
+  position: relative;
+  min-height: 100vh;
+}
+</style>
